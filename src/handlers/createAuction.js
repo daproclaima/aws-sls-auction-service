20642944503048300510e0ec9a72bcaf1426b86e
daproclaima <sn.itpro@gmail.com>
2020-12-31 22:46:05 +0100

    refactor(createAuction): add validator

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,10 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import createError from "http-errors";
+import validator from '@middy/validator';
 import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
+import JSONErrorHandlerMiddleware from 'middy-middleware-json-error-handler';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -38,4 +41,8 @@ async function createAuction(event, _) {
     };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use([
+    validator({ inputSchema: createAuctionSchema }),
+    JSONErrorHandlerMiddleware(),
+  ]);
