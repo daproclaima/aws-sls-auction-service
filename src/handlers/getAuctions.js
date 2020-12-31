576a9709c58b8e643fa417ef3182905921b553a6
daproclaima <sn.itpro@gmail.com>
2020-12-31 17:19:24 +0100

    refactor(getAuctions): change function to use query instead of scan

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -6,13 +6,23 @@ import createError from "http-errors";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
-
+  const { status } = event.queryStringParameters;
   let auctions;
 
-  try{
-    const result = await dynamodb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-    }).promise();
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
+
+  try {
+    const result = await dynamodb.query(params).promise();
     auctions = result.Items;
   } catch(error) {
     console.log(error);
