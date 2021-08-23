cf2fef501e48fdf70e3a5e067c9b1d9297e46383
daproclaima <sn.itpro@gmail.com>
2021-08-23 17:55:30 +0200

    fix syntax on lib files

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -2,6 +2,7 @@ import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const sqs = new AWS.SQS();
+
 export async function closeAuction(auction) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
@@ -30,23 +31,23 @@ export async function closeAuction(auction) {
     }).promise();
   }
 
-    const notifySeller = sqs.sendMessage({
-      QueueUrl: process.env.MAIL_QUEUE_URL,
-      MessageBody: JSON.stringify({
-        subject: `Your item ${ title } has been sold!`,
-        recipient: seller,
-        body: `Amazing! Your item ${ title } has been sold for $${ amount }.`,
-      }),
-    }).promise();
+  const notifySeller = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: `Your item ${ title } has been sold!`,
+      recipient: seller,
+      body: `Amazing! Your item ${ title } has been sold for $${ amount }.`,
+    }),
+  }).promise();
 
-    const notifyBidder = sqs.sendMessage({
-      QueueUrl: process.env.MAIL_QUEUE_URL,
-      MessageBody: JSON.stringify({
-        subject: 'You won an auction!',
-        recipient: bidder,
-        body: `What a great deal! You got yourself a ${ title } for $${ amount }.`,
-      }),
-    }).promise();
+  const notifyBidder = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: 'You won an auction!',
+      recipient: bidder,
+      body: `What a great deal! You got yourself a ${ title } for $${ amount }.`,
+    }),
+  }).promise();
 
-    return Promise.all([ notifySeller, notifyBidder ]);
+  return Promise.all([ notifySeller, notifyBidder ]);
 };
